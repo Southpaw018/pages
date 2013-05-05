518c7ee93e449950f98178269b4a068ec1cc665a
Jim Drey <moof@moofit.com>
2013-05-05 17:11:24 -0400

    Fix bugged XML save with CDATA block

--- index.php
+++ index.php
@@ -117,6 +117,7 @@
 		@$accuweather->loadHTMLFile("http://www.accuweather.com/en/us/takoma-park-md/20912/current-weather/2135463");
 		if ($accuweather->getElementById('detail-now')) {$current = $accuweather->saveXML($accuweather->getElementById('detail-now'));}
 		$current = str_replace("&#13;","",$current);
+		$current = str_replace(array('<![CDATA[',']]>'), array('//<![CDATA[','//]]>'), $current);
 		//$current = str_replace("src=\"/adc2010/images/icons-wind/","src=\"http://www.accuweather.com/adc2010/images/icons-wind/",$current);
 		echo($current);
 		?>
